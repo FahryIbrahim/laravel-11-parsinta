524cc39c73c40d45fb3a4c3dc4ccbaea1e0414b5
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-17 14:00:47 +0700

    feat: make dynamic title

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
+Route::view('/gallery', 'gallery');
