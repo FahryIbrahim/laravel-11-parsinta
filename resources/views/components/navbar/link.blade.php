629b7b364c4936b6b6dd8d284dd37e07ef06c812
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-17 14:47:34 +0700

    feat: make hover on the navbar

--- resources/views/components/navbar/link.blade.php
+++ resources/views/components/navbar/link.blade.php
@@ -1,3 +1,2 @@
-<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-
-<a {{$attributes}} class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{$slot}}</a>
+<a {{ $attributes }}
+    class="{{ request()->fullUrlIs(url($href)) ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">{{ $slot }}</a>
