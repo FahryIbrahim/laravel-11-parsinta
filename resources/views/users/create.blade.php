6ccc345c770653c08e8256dd19a826506eeb5f44
Fahry Ibrahim <fahryibrahim06@gmail.com>
2024-08-18 20:43:28 +0700

    feat: add validation on store method and add id language for validation,auth,pagination,password

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,14 +8,23 @@
         <div class="">
             <label for="name">Name</label>
             <input class="border py-2 px-4 rounded block md-1" type="text" name="name" id="name">
+            @error('name')
+                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
         </div>
         <div class="">
             <label for="email">Email</label>
             <input class="border py-2 px-4 rounded block md-1" type="email" name="email" id="email">
+            @error('email')
+                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
         </div>
         <div class="">
             <label for="password">Password</label>
             <input class="border py-2 px-4 rounded block md-1" type="password" name="password" id="password">
+            @error('password')
+                <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
+            @enderror
         </div>
         <x-button>
             Save
